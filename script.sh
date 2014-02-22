3de91645637ea0e272986f7586010fbf09d46bda
jnaour <jnaour@jnaour-desktop.home>
2014-02-22 10:59:15 +0100

    Change directory to gnome wallpapers directory

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DIR="/home/jnaour/Pictures/Wallpapers"
+DIR="/usr/share/backgrounds/gnome"
 SLEEPING_TIME=600
 
 while true;do
-	PIC=$(ls $DIR/*.png | shuf -n1)
+	PIC=$(ls $DIR/* | shuf -n1)
 	gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
 	sleep $SLEEPING_TIME
 	done
